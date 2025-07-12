1018981c4e201379777da744a8d0399a7fd06bbd
akibur-r <akibur.pciu@gmail.com>
2025-07-12 11:41:45 +0600

    Added footer

--- src/components/root/Footer/Footer.tsx
+++ src/components/root/Footer/Footer.tsx
@@ -1,5 +1,44 @@
+import Logo from "@/components/shared/Logo/Logo";
+import { BsTwitterX } from "react-icons/bs";
+import { FiFacebook, FiLinkedin } from "react-icons/fi";
+import { Link } from "react-router";
+
 const Footer = () => {
-  return <div>foot</div>;
+  return (
+    <footer className="max-w-screen-xl mx-auto px-4 py-2 flex flex-col gap-4">
+      <div className=" flex flex-col md:flex-row gap-y-4 justify-between items-center">
+        <div className="h-14">
+          <Logo showText />
+        </div>
+        <div className="flex gap-2 items-end text-xl">
+          <Link
+            target="_blank"
+            to={"/"}
+            className="hover:text-primary transition-all duration-100"
+          >
+            <FiFacebook />
+          </Link>
+          <Link
+            target="_blank"
+            to={"/"}
+            className="hover:text-primary transition-all duration-100"
+          >
+            <FiLinkedin />
+          </Link>
+          <Link
+            target="_blank"
+            to={"/"}
+            className="hover:text-primary transition-all duration-100"
+          >
+            <BsTwitterX className="text-base" />
+          </Link>
+        </div>
+      </div>
+      <div className="text-xs border-t border-t-foreground/10 dark:border-t-foreground/5 py-1 text-center md:text-left">
+        © 2025 by Tasklet LLC. All Rights Reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
